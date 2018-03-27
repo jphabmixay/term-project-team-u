de8ff8bb3024c7a5505279838ec4a413a5a3a9f6
Ivan <villamar.ivan@yahoo.com>
2018-03-27 00:00:04 -0700

    Attempt at adding a tests route, currently not working properly

---
+++ myapp-uno/routes/tests.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const db = require('../db');
+
+router.get("/", (request, response) => {
+  res.send('tests route is working!');
+  /*
+  db.any(`INSERT INTO test_table ("testString") VALUES ('Hello at ${Date.now()}')`)
+    .then( _ => db.any(`SELECT * FROM test_table`) )
+    .then( results => response.json( results ) )
+    .catch( error => {
+      console.log( error )
+      response.json({ error })
+    })
+  */
+});
+
+module.exports = router;
+
