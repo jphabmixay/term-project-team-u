c353863778aa48f00f65b3132b932c923531d6eb
Ivan <villamar.ivan@yahoo.com>
2018-04-18 16:31:35 -0700

    created the routes for login and register page

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Nothing to do here, hehe!');
+});
+
+router.get('/login', (request, response) => {
+  response.send('this will be the login page!');
+});
+
+router.get('/register', (request, response) => {
+  response.send('this will be the register page!');
 });
 
 module.exports = router;
