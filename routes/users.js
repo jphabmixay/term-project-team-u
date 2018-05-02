ecd22429124ce6278454dc3ca0301893508a8930
Joe Phabmixay <jphabmixay@gmail.com>
2018-05-01 20:09:39 -0700

    adding route for successful login page.

--- routes/users.js
+++ routes/users.js
@@ -21,6 +21,10 @@ router.get('/register', (request, response) => {
   response.render('register');
 });
 
+router.get('/loginsuccess', (request, response) => {
+  response.render('loginsuccess');
+});
+
 router.post('/register', (request, response, next) => {
   const { email, password } = request.body;
 
