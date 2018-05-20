aa8a6cd260499c7e3d5aa4113f864bfcfec83df2
root <jphabmixay@gmail.com>
2018-05-20 15:34:01 -0700

    Revert "Cleaned up testing code."
    This reverts commit 435769b41e85e5c0bf458c932972bef8b6428f67.

--- controllers/socket-server/index.js
+++ controllers/socket-server/index.js
@@ -1,44 +1,72 @@
 const socketIo = require('socket.io');
+
 const eventHandler = require('../game');
+
 const lobbyController = require('../lobby');
+
 const GAMESERVER = 'game';
+
+
 const LOBBYSERVER = 'lobby server';
 
 
 const socketServer = (app, server) => {
-	const io = socketIo(server);
+  const io = socketIo(server);
 
-	app.set('io', io);
+  app.set('io', io);
 
-	io.on('connection', socket => {
-			console.log('game client connected');
-			socket.on('disconnect', data => {
-					console.log('game client disconnected');
-			});
+  io.on('connection', socket => {
+      console.log('game client connected');
 
-			socket.on('chat message', function(msg) {
-				console.log('Message:' + msg);
-				socket.emit('chat message', msg);
-			});
+      socket.on('disconnect', data => {
+          console.log('game client disconnected');
+      });
 
-			socket.on('game', function(msg) {
-			console.log('server received ', JSON.stringify(msg))
-			eventHandler(msg, function(toPlayer, toGroup) {
-					socket.emit('game', toPlayer);
-					io.emit('game', toGroup);
-				})
-			});
+     socket.on('chat message', function(msg) {
+        console.log('Message:' + msg);
+        socket.emit('chat message', msg);
+      });
+
+     socket.on('game', function(msg) {
+      console.log('server received ', JSON.stringify(msg))
+  
+      eventHandler(msg, function(toPlayer, toGroup) {
+          socket.emit('game', toPlayer);
+
+          io.emit('game', toGroup);
+        })
+      });
+
+      
+	  socket.on(LOBBYSERVER, function(msg) 
+{
+        // next line is for testing use
+        console.log('server received ', JSON.stringify(msg));
 	
-			socket.on('game chat', function(msg) {
-					console.log('server received ', JSON.stringify(msg));
-					mockGameController(msg).then(ret => {	
-						socket.emit(GAMESERVER, ret.player);
-						io.sockets.emit(GAMESERVER, ret.group);
-					}).catch( error => {
-					console.log(error);
-					});
-			});
-	});
+	  lobbyController(msg).then(ret => {	
+			console.log(ret.group.games);
+        
+  	socket.emit(LOBBYSERVER, ret.player);
+          
+	io.sockets.emit(LOBBYSERVER, ret.group);
+
+		  }).catch( error => {
+			console.log(error);
+		  });
+      });
+	
+  socket.on('game chat', function(msg) {
+        // next line is for testing use
+        console.log('server received ', JSON.stringify(msg));
+		  mockGameController(msg).then(ret => {	
+          	socket.emit(GAMESERVER, ret.player);
+      
+    	io.sockets.emit(GAMESERVER, ret.group);
+		  }).catch( error => {
+			console.log(error);
+		  });
+      });
+  });
 }
 
 module.exports = socketServer;
